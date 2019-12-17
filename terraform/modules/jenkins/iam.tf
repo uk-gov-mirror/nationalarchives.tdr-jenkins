dc95ef592a4c1fb8153aca6a10b023b57c4f1883
Sam Palmer <github@sampalmer.dev>
2019-12-17 15:43:17 +0000

    Add PassRole permission to fargate execution role

--- terraform/modules/jenkins/iam.tf
+++ terraform/modules/jenkins/iam.tf
@@ -38,9 +38,13 @@ data "aws_iam_policy_document" "jenkins_fargate_policy_document" {
     effect = "Allow"
     actions = [
       "ecs:StopTask",
-      "ecs:ListContainerInstances"
+      "ecs:ListContainerInstances",
+      "iam:PassRole"
     ]
-    resources = ["arn:aws:ecs:eu-west-2:${data.aws_caller_identity.current.account_id}:cluster/${aws_ecs_cluster.jenkins_cluster.name}"]
+    resources = [
+      "arn:aws:ecs:eu-west-2:${data.aws_caller_identity.current.account_id}:cluster/${aws_ecs_cluster.jenkins_cluster.name}",
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TDRJenkinsNodeRole"
+    ]
   }
 
   statement {
