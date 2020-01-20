377bd5af4224d1b6f8ee12a668e0f6062494d4c7
Sam Palmer <github@sampalmer.dev>
2020-01-20 13:15:24 +0000

    Make the upload more generic

--- docker/aws/upload_to_s3.py
+++ docker/aws/upload_to_s3.py
@@ -3,9 +3,11 @@ import os
 import boto3
 
 stage = sys.argv[1]
+directory = sys.argv[2]
+bucket_name = sys.argv[3]
 
 client = boto3.client('s3')
 
-for file_name in os.listdir("migrations"):
-    client.upload_file('migrations/' + file_name, 'tdr-database-migrations', stage + '/' + file_name)
+for file_name in os.listdir(directory):
+    client.upload_file( directory + '/' + file_name,bucket_name,stage + '/' + file_name)
 
