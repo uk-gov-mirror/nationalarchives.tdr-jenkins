5a93fda9e63c1df9eafb835593937017a221da1e
TomJKing <Tom.King@nationalarchives.gov.uk>
2020-01-29 10:29:56 +0000

    Add script to retrieve ssm param
    To run end to end tests need to create test users
    
    To create users need to use keycloak api authenticated using admin credentials which are stored as ssm parameters

---
+++ docker/aws/ssm_get_parameter.py
@@ -0,0 +1,13 @@
+import sys
+from sessions import get_session
+
+account_number = sys.argv[1]
+stage = sys.argv[2]
+param_name = sys.argv[3]
+
+boto_session = get_session(account_number, "TDRJenkinsReadParamsRole" + stage.capitalize())
+client = boto_session.client('ssm', region_name='eu-west-2')
+
+parameter = client.get_parameters(Names=[param_name], WithDecryption=True)
+
+print(parameter['Parameters'][0]['Value'])
