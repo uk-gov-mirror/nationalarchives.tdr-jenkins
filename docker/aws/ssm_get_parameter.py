aed3c82c2e9b1ad01ea4f02c8e9d0cdf963dced8
TomJKing <Tom.King@nationalarchives.gov.uk>
2020-02-04 11:17:33 +0000

    Add comment that ssm param script will print to output
    ssm param python script will print the value of the param to output
    
    Add comment to this affect along with advice as to how to hide the output in console if required

--- docker/aws/ssm_get_parameter.py
+++ docker/aws/ssm_get_parameter.py
@@ -1,6 +1,13 @@
 import sys
 from sessions import get_session
 
+# This script will retrieve the SSM param and prints the value
+# Too ensure the value does not appear in the console run the script with set+x command this will hide the output on the console:
+# sh """
+#   set +x
+#   [command that uses script]
+# """
+
 account_number = sys.argv[1]
 stage = sys.argv[2]
 param_name = sys.argv[3]
