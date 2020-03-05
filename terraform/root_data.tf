34539574b5b83e3833542d2248e95d8d6abb5f22
Paul Schwarzenberger <paul@celidor.net>
2020-03-05 14:05:41 +0000

    added comment and Terraform format

--- terraform/root_data.tf
+++ terraform/root_data.tf
@@ -8,6 +8,7 @@ data "aws_ssm_parameter" "cost_centre" {
 }
 
 data "aws_ami" "ecs_ami" {
+  # look for the latest public Amazon ECS image from Amazon owned account
   owners      = ["591542846629"]
   name_regex  = "^amzn2-ami-ecs-hvm-2.0.\\d{8}-x86_64-ebs"
   most_recent = true
--- terraform/root_locals.tf
+++ terraform/root_locals.tf
@@ -3,10 +3,10 @@ locals {
   tag_prefix  = var.tag_prefix
   aws_region  = var.default_aws_region
   common_tags = map(
-  "Environment", local.environment,
-  "Owner", "TDR",
-  "Terraform", true,
-  "CostCentre", data.aws_ssm_parameter.cost_centre.value
+    "Environment", local.environment,
+    "Owner", "TDR",
+    "Terraform", true,
+    "CostCentre", data.aws_ssm_parameter.cost_centre.value
   )
   secrets_file_content = data.aws_s3_bucket_object.secrets.body
   secrets              = yamldecode(local.secrets_file_content)
