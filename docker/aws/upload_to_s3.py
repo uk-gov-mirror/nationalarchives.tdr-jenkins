2ef0fcf28fc036059850050f8574ebc17aa8bcfb
sam <github@sampalmer.dev>
2020-05-14 15:14:31 +0100

    Add new python scripts.
    One is to upload the code to S3 and one is to deploy the lambda from S3.

---
+++ docker/aws/upload_to_s3.py
@@ -0,0 +1,16 @@
+import sys
+from sessions import get_session
+
+account_number = sys.argv[1]
+stage = sys.argv[2]
+s3_bucket = sys.argv[3]
+s3_key = sys.argv[4]
+filename = sys.argv[5]
+
+boto_session = get_session(account_number, "TDRJenkinsS3Role" + stage.capitalize())
+
+client = boto_session.client("s3")
+
+response = client.upload_file(filename, s3_bucket, s3_key)
+
+print(response['ResponseMetadata']['RequestId'])
