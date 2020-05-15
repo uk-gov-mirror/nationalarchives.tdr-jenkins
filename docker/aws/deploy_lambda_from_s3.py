874a88a42e7b5fe45cb31f0869178708a6239739
sam <github@sampalmer.dev>
2020-05-15 14:49:46 +0100

    Update the event mapping with the new version.
    This means that the message from sqs queue will always hit a numbered
    version rather than $LATEST which means that we can use this version
    within the lambda code.

--- docker/aws/deploy_lambda_from_s3.py
+++ docker/aws/deploy_lambda_from_s3.py
@@ -13,4 +13,9 @@ client = boto_session.client("lambda")
 
 resp = client.update_function_code(FunctionName=function_name, S3Bucket=s3_bucket, S3Key=s3_key)
 response = client.publish_version(FunctionName=function_name)
-print("v" + response["Version"])
+function_arn = 'arn:aws:lambda:eu-west-2:' + account_number + ':function:' + function_name
+event_mappings = client.list_event_source_mappings()['EventSourceMappings']
+version = response["Version"]
+uuid = list(filter(lambda x: x['FunctionArn'].startswith(function_arn), event_mappings))[0]['UUID']
+client.update_event_source_mapping(UUID=uuid, FunctionName=function_arn + ":" + version)
+print("v" + version)
