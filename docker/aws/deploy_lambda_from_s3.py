c583c77c0f393e38c691cf1630b1c1cc5c7ed692
sam <github@sampalmer.dev>
2020-05-15 14:00:19 +0100

    Returns the published version
    The script publishes a new version and returns this version. This can be
    used for git tagging or other purposes.

--- docker/aws/deploy_lambda_from_s3.py
+++ docker/aws/deploy_lambda_from_s3.py
@@ -6,13 +6,11 @@ stage = sys.argv[2]
 function_name = sys.argv[3]
 s3_bucket = sys.argv[4]
 s3_key = sys.argv[5]
-version_tag = sys.argv[6]
 
 boto_session = get_session(account_number, "TDRJenkinsLambdaRole" + stage.capitalize())
 
 client = boto_session.client("lambda")
 
 resp = client.update_function_code(FunctionName=function_name, S3Bucket=s3_bucket, S3Key=s3_key)
-response = client.create_alias(FunctionName=function_name, Name=version_tag, FunctionVersion='latest')
-
-print(resp['ResponseMetadata']['RequestId'])
+response = client.publish_version(FunctionName=function_name)
+print("v" + response["Version"])
