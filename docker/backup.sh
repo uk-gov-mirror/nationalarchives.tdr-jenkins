a02061abcb13ca7b616cedabdd96ad25f7b9cf1a
sam <sam.palmer@nationalarchives.gov.uk>
2020-10-19 08:32:30 +0100

    Add line break

--- docker/backup.sh
+++ docker/backup.sh
@@ -4,3 +4,4 @@ rm -rf jobs jenkins-backup.tar.gz
 cp -R /var/jenkins_home/jobs .
 tar -zcf jenkins-backup.tar.gz jobs
 aws s3 cp jenkins-backup.tar.gz s3://tdr-jenkins-backup-mgmt/jenkins-backup-`date +"%Y-%m-%d:%H:%M"`.tar.gz
+
