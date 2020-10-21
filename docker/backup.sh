9375c24ac048eba03592dbbe7ebf10c8ebf74cb0
sam <sam.palmer@nationalarchives.gov.uk>
2020-10-21 09:41:16 +0100

    Add call to healthcheck.io
    Healtcheck.io will alert us if this url isn't pinged at least once a day
    by this script.
    The $1 argument here is the healthcheck url which is passed in through
    terraform.

--- docker/backup.sh
+++ docker/backup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+curl $1/start
 rm -rf jobs jenkins-backup.tar.gz
 cp -R /var/jenkins_home/jobs .
 tar -zcf jenkins-backup.tar.gz jobs
 aws s3 cp jenkins-backup.tar.gz s3://tdr-jenkins-backup-mgmt/jenkins-backup-`date +"%Y-%m-%d:%H:%M"`.tar.gz
+curl $1
 
