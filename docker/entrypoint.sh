b3d79908938875dd8c8fa31bbcc5aa9068b21a52
TomJKing <tom.king@nationalarchives.gov.uk>
2020-12-09 13:44:19 +0000

    Change copy location of backup tar.gz file
    Copy the backup tar.gz to a directory where the jenkins user has read / write permissions to allow unpacking
    
    Jenkins user does not have read/ write permissions on the root directory

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FILENAME=$(aws s3 ls s3://tdr-jenkins-backup-mgmt | awk '{print $4}' | sort -r | head -1)
-aws s3 cp s3://tdr-jenkins-backup-mgmt/"$FILENAME" jenkins-backup.tar.gz
-tar xzf jenkins-backup.tar.gz -C /var/jenkins_home
+# copy backup tar.gz file to var/jenkins_home directory so jenkins user has permissions to copy
+aws s3 cp s3://tdr-jenkins-backup-mgmt/"$FILENAME" /var/jenkins_home/jenkins-backup.tar.gz
+tar xzf /var/jenkins_home/jenkins-backup.tar.gz -C /var/jenkins_home
 /sbin/tini -- /usr/local/bin/jenkins.sh
