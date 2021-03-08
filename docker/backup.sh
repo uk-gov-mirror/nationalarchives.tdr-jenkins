f43d11835bbdbb2a8136ff66b159e2188c57a1de
Sam Palmer <github@sampalmer.dev>
2021-03-08 17:28:29 +0000

    Stream backup file straight to s3
    Instead of having tar write the file to disk then copy the file to s3,
    tar will stream to stdout and then the aws cli streams stdout to s3.
    This means even if we're low on disk space, the backup will complete.

--- docker/backup.sh
+++ docker/backup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 curl $1/start
-tar -zcf /var/jenkins_home/jenkins-backup.tar.gz -C /var/jenkins_home jobs
-aws s3 cp /var/jenkins_home/jenkins-backup.tar.gz s3://tdr-jenkins-backup-mgmt/jenkins-backup-`date +"%Y-%m-%d:%H:%M"`.tar.gz
-rm -f /var/jenkins_home/jenkins-backup.tar.gz
+tar -zcf - -C /var/jenkins_home jobs | aws s3 cp - s3://tdr-jenkins-backup-mgmt/jenkins-backup-`date +"%Y-%m-%d:%H:%M"`.tar.gz
 curl $1
-
