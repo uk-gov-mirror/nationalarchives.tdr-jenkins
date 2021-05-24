33796791385f9b89bdf1fc0192aed26607f58ba8
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-05-24 12:12:36 +0100

    Use the correct bucket name in the entrypoint script

--- docker/entrypoint-prod.sh
+++ docker/entrypoint-prod.sh
@@ -2,7 +2,7 @@
 
 FILENAME=$(aws s3 ls s3://tdr-jenkins-backup-prod-mgmt | awk '{print $4}' | sort -r | head -1)
 # copy backup tar.gz file to var/jenkins_home directory so jenkins user has permissions to copy
-aws s3 cp s3://tdr-jenkins-backup-mgmt/"$FILENAME" /var/jenkins_home/jenkins-backup.tar.gz
+aws s3 cp s3://tdr-jenkins-backup-prod-mgmt/"$FILENAME" /var/jenkins_home/jenkins-backup.tar.gz
 tar xzf /var/jenkins_home/jenkins-backup.tar.gz -C /var/jenkins_home
 rm -f /var/jenkins_home/jenkins-backup.tar.gz
 git-secrets --register-aws --global
