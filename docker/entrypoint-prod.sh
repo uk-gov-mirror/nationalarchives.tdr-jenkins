17b9a3e2b6bf09e7bdb797a235be0258d3390763
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-05-24 10:13:23 +0100

    Add production versions of the scripts
    They only vary slightly mostly in the bucket name for the backup but
    this is clearer and safer I think than passing in bucket arguments to
    the existing scripts.

--- docker/entrypoint-prod.sh
+++ docker/entrypoint-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILENAME=$(aws s3 ls s3://tdr-jenkins-backup-mgmt | awk '{print $4}' | sort -r | head -1)
+FILENAME=$(aws s3 ls s3://tdr-jenkins-backup-prod-mgmt | awk '{print $4}' | sort -r | head -1)
 # copy backup tar.gz file to var/jenkins_home directory so jenkins user has permissions to copy
 aws s3 cp s3://tdr-jenkins-backup-mgmt/"$FILENAME" /var/jenkins_home/jenkins-backup.tar.gz
 tar xzf /var/jenkins_home/jenkins-backup.tar.gz -C /var/jenkins_home
