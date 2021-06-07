747316b11bd73440f319e11a5bab5a01325e2a4e
TomJKing <tom.king@nationalarchives.gov.uk>
2021-06-07 11:11:42 +0100

    Attach Terraform v13 policies to ECS execution role

--- terraform/jenkins_terraform_v13_node.tf
+++ terraform/jenkins_terraform_v13_node.tf
@@ -4,7 +4,7 @@ module "jenkins_ecr_build_terraform_v13_repository" {
   image_source_url = "https://github.com/nationalarchives/tdr-jenkins/blob/master/docker/terraform-v13/Dockerfile"
   common_tags      = local.common_tags
   policy_name      = "jenkins_policy"
-  policy_variables = { role_arn = module.jenkins_build_terraform_execution_role.role.arn }
+  policy_variables = { role_arn = module.jenkins_build_terraform_v13_execution_role.role.arn }
 }
 
 module "jenkins_build_terraform_v13_execution_policy" {
@@ -18,5 +18,5 @@ module "jenkins_build_terraform_v13_execution_role" {
   assume_role_policy = templatefile("./tdr-terraform-modules/ecs/templates/ecs_assume_role_policy.json.tpl", {})
   common_tags        = local.common_tags
   name               = "TDRJenkinsBuildTerraformV13ExecutionRole"
-  policy_attachments = { ecr_policy = module.jenkins_build_terraform_execution_policy.policy_arn }
+  policy_attachments = { ecr_policy = module.jenkins_build_terraform_v13_execution_policy.policy_arn }
 }
