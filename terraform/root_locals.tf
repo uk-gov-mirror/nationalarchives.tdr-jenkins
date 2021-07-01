2f2c50844cd36d6c7c33a0e76742d0db8534aeff
TomJKing <tom.king@nationalarchives.gov.uk>
2021-07-01 09:59:44 +0100

    Change 'map' constructor to 'tomap' constructor

--- terraform/root_locals.tf
+++ terraform/root_locals.tf
@@ -2,12 +2,14 @@ locals {
   environment = "mgmt"
   tag_prefix  = var.tag_prefix
   aws_region  = var.default_aws_region
-  common_tags = map(
-    "Environment", local.environment,
-    "Owner", "TDR",
-    "Terraform", true,
-    "TerraformSource", "https://github.com/nationalarchives/tdr-jenkins/tree/master/terraform",
-    "CostCentre", data.aws_ssm_parameter.cost_centre.value
+  common_tags = tomap(
+    {
+      "Environment"     = local.environment,
+      "Owner"           = "TDR",
+      "Terraform"       = true,
+      "TerraformSource" = "https://github.com/nationalarchives/tdr-jenkins/tree/master/terraform",
+      "CostCentre"      = data.aws_ssm_parameter.cost_centre.value
+    }
   )
   ec2_instance_name = "JenkinsTaskDefinition"
 
