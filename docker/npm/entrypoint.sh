05813ce0bf23dd837db24257653fa58ea23919f2
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-08-05 12:44:45 +0100

    Get three containers set up to sign commits.
    There's three parts to this.
    
    * Create the task definitions which will be used by Jenkins for these nodes.
    * Create the policies that will be used by these task definitions.
    * Change the docker files to use the custom entrypoint which sets up the gpg keys

---
+++ docker/npm/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git config --global user.signingkey $GPG_KEY_ID
+git config --global commit.gpgsign true
+gpg-agent --allow-preset-passphrase --daemon
+chown -R jenkins:jenkins /home/jenkins/.gnupg && find /home/jenkins/.gnupg -type f -exec chmod 600 {} \; && find /home/jenkins/.gnupg -type d -exec chmod 700 {} \;
+echo -e "$GPG_KEY" | gpg --batch --import
+echo $GPG_KEY_ID:6: | gpg --import-ownertrust
+export KEYGRIP=$(gpg --list-keys --with-keygrip | grep Keygrip | cut -d'=' -f2 | xargs)
+/usr/libexec/gpg-preset-passphrase -c $KEYGRIP <<< $PASSPHRASE
+unset PASSPHRASE KEYGRIP GPG_KEY GPG_KEY_ID
+/usr/local/bin/jenkins-agent "$@"
--- terraform/root_common.tf
+++ terraform/root_common.tf
@@ -10,6 +10,14 @@ module "sbt_with_postgres" {
   sbt_with_postgres = true
 }
 
+module "plugin_updates" {
+  source         = "./tdr-terraform-modules/ecs"
+  common_tags    = local.common_tags
+  project        = var.project
+  vpc_id         = module.jenkins_vpc.vpc_id
+  plugin_updates = true
+}
+
 module "encryption_key" {
   source      = "./tdr-terraform-modules/kms"
   project     = var.project
@@ -133,4 +141,10 @@ module "jenkins_flow_logs" {
   role_arn      = module.jenkins_flow_log_role.role.arn
   s3_arn        = "arn:aws:s3:::tdr-log-data-mgmt/flowlogs/${local.environment}/jenkins/"
   vpc_id        = module.jenkins_vpc.vpc_id
+}
+
+module "jenkins_sign_commits_policy" {
+  source        = "./tdr-terraform-modules/iam_policy"
+  name          = "TDRJenkinsSignCommitsPolicy"
+  policy_string = templatefile("./tdr-terraform-modules/iam_policy/templates/jenkins_github_gpg_policy.json.tpl", { account_id = data.aws_caller_identity.current.account_id })
 }
